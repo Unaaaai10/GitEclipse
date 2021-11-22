3e31b4aa44620cf88463e00c7c4036bd101602bf
Katsu <abuenocr21dw@ikzubirimanteo.com>
2021-11-22 12:40:22 +0100

    Añado dos clases más

---
+++ ED/src/GitEnIDE/prueba2.java
@@ -0,0 +1,11 @@
+package GitEnIDE;
+
+public class prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Segunda clase");
+
+	}
+
+}
---
+++ ED/src/GitEnIDE/prueba3.java
@@ -0,0 +1,11 @@
+package GitEnIDE;
+
+public class prueba3 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Tercera clase, segundo commit");
+
+	}
+
+}
