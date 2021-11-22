dedffb81cb841f0681d8ebbe46390c1fa6f88f51
PabloTC <ptorresca21dw@ikzubirimanteo.com>
2021-11-22 12:48:01 +0100

    Creo nueva rama y nueva clase en esa rama

---
+++ ED/src/GitEnIDE/prueba4.java
@@ -0,0 +1,11 @@
+package GitEnIDE;
+
+public class prueba4 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Nueva rama, nueva clase");
+
+	}
+
+}
